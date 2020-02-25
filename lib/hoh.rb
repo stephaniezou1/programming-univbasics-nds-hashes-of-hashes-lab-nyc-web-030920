bd699d32ee61108cc04ee2dcb6a9c074d6b9fd68
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-25 16:02:42 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -37,25 +37,25 @@ def naming_system
      label: "Phylum",
      sub_category: {
        label: "Class"
-     }
-   }
- }
+      }
+    }
+  }
  
   PORTION_2 = {
    label: "Order"
- }
+  }
 
   PORTION_3 = {
    label: "Family",
    sub_category: {
      label: "Genus",
-   }
- }
+    }
+  }
  
   PORTION_4 = {
    label: "Species",
    sub_category: nil
- }
+  }
   # Remember:
   #  Kingdom
   #  Phylum
